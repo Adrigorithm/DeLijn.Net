80aac662c297468d9140025e23851f6a8bc3c89e
Adrigorithm <2ndalgorithm@gmail.com>
2024-06-06 17:41:45 +0200

    now I am afraid to test this manual labour :wires:

--- App/Api/Static/ApiEndpoints.cs
+++ App/Api/Static/ApiEndpoints.cs
@@ -37,7 +37,7 @@ internal static class ApiEndpoints
         $"{GetAllEntities}/{entityId}/gemeenten";
 
     internal static string GetStop(short entityId, short stopId, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/{entityId}/{stopId}?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetStopsByMunicipality(short municipalityId) =>
         $"{GetAllMunicipalities}/{municipalityId}/haltes";
@@ -46,7 +46,7 @@ internal static class ApiEndpoints
         $"{GetAllEntities}/{entityId}/haltes";
 
     internal static string GetDiversions(DateTimeOffset? startDate = null, DateTimeOffset? endDate = null) =>
-        $"{GetAllDiversions}?eindDatum={endDate.ToDeLijnDateTimeOffsetString(true)}&startDatum={startDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllDiversions}", ("eindDatum", endDate.ToDeLijnDateTimeOffsetString(true)), ("startDatum", startDate.ToDeLijnDateTimeOffsetString(true)));
 
     /// <summary>
     /// return all lines currently or in the future valid for the requested entity
@@ -54,7 +54,7 @@ internal static class ApiEndpoints
     /// <param name="entityId">filter by entity</param>
     /// <param name="validOnDate">filter by a date on which all returned lines should be valid</param>
     internal static string GetLinesByEntity(short entityId, DateTimeOffset? validOnDate) =>
-        $"{GetAllEntities}/{entityId}/lijnen?geldigOp={validOnDate.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllEntities}/{entityId}/lijnen", ("geldigOp", validOnDate.ToDeLijnDateOnlyString(true)));
 
     internal static string GetLinesByMunicipality(short municipalityId) =>
         $"{GetAllMunicipalities}/{municipalityId}/lijnen";
@@ -63,80 +63,80 @@ internal static class ApiEndpoints
         $"{GetAllMunicipalities}/{id}";
 
     internal static string GetTimetableForStop(short entityId, short stopId, DateTimeOffset? date) =>
-        $"{GetAllStops}/{entityId}/{stopId}/dienstregelingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}/dienstregelingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetLineDirectionsForStop(short entityId, short stopId, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/{entityId}/{stopId}/lijnrichtingen?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}/lijnrichtingen", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetDiversionsByStop(short entityId, short stopId, DateTimeOffset? date) =>
-        $"{GetAllStops}/{entityId}/{stopId}/omleidingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}/omleidingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetLiveDataForStop(short entityId, short stopId, short? maxResults) =>
-        $"{GetAllStops}/{entityId}/{stopId}/real-time?maxAantalDoorkomsten={maxResults}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}/real-time", ("maxAantalDoorkomsten", maxResults.ToString()));
 
     internal static string GetDisruptionsByStop(short entityId, short stopId, DateTimeOffset? date) =>
-        $"{GetAllStops}/{entityId}/{stopId}/storingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/{entityId}/{stopId}/storingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetStopPointsNearCoordinate(GeoCoordinate coordinate, short? maxResults, short? radius) =>
-        $"{GetAllStops}/indebuurt/{coordinate}?maxAantalHaltes={maxResults}&radius={radius}";
+        AddOptionalParameters($"{GetAllStops}/indebuurt/{coordinate}", ("maxAantalHaltes", maxResults.ToString()), ("radius", radius.ToString()));
 
     internal static string GetStopsByKeys(IEnumerable<short> stopIds, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetTimetableForStopKeys(IEnumerable<short> stopIds, DateTimeOffset? date) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/dienstregelingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/dienstregelingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetLineDirectionForStopKeys(IEnumerable<short> stopIds, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}?lijnrichtingen?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/lijnrichtingen", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetDiversionsForStopKeys(IEnumerable<short> stopIds, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/omleidingen?datum={validOnDate.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/omleidingen", ("datum", validOnDate.ToDeLijnDateOnlyString(true)));
 
     internal static string GetRealTimeForStopKeys(IEnumerable<short> stopIds, short? maxResults) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/real-time?maxAantalDoorkomsten={maxResults}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/real-time", ("maxAantalDoorkomsten", maxResults.ToString()));
 
     internal static string GetDisruptionsForStopKeys(IEnumerable<short> stopIds, DateTimeOffset? validOnDate) =>
-        $"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/storingen?datum={validOnDate.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllStops}/lijst/{stopIds.ToStopIdUrlParamList()}/storingen", ("datum", validOnDate.ToDeLijnDateOnlyString(true)));
 
     internal static string GetColourByCode(string code) =>
         $"{GetAllColours}/{code}";
 
     internal static string GetLineById(short entityId, short lineId, DateTimeOffset? validOnDate) =>
-        $"{GetAllLines}/{entityId}/{lineId}?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetMunicipalitiesServedByLine(short entityId, short lineId, DateTimeOffset? validOnDate) =>
-        $"{GetAllLines}/{entityId}/{lineId}/gemeenten?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/gemeenten", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetColoursForLine(short entityId, short lineId, DateTimeOffset? validOnDate) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnkleuren?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnkleuren", ("geldigOp",validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetLineDirectionsForLine(short entityId, short lineId, DateTimeOffset? validOnDate) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen?geldigOp={validOnDate.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen", ("geldigOp", validOnDate.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetLineDirectionsForLineDirection(short entityId, short lineId, Direction lineDirection, DateTimeOffset? validOnDate) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}?geldigOp={validOnDate.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}", ("geldigOp", validOnDate.ToDeLijnDateOnlyString(true)));
 
     internal static string GetTimetableForLineDirection(short entityId, short lineId, Direction lineDirection, DateTimeOffset? date) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/dienstregelingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/dienstregelingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetStopsForLineDirection(short entityId, short lineId, Direction lineDirection, DateTimeOffset? date) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/haltes?geldigOp={date.ToDeLijnDateTimeOffsetString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/haltes", ("geldigOp", date.ToDeLijnDateTimeOffsetString(true)));
 
     internal static string GetDiversionsForLineDirection(short entityId, short lineId, Direction lineDirection, DateTimeOffset? date) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/omleidingen?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/omleidingen", ("datum", date.ToDeLijnDateOnlyString(true)));
 
     internal static string GetRealTimeForLineDirection(short entityId, short lineId, Direction lineDirection) =>
         $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/real-time";
 
     internal static string GetDrivesForLineDirection(short entityId, short lineId, Direction lineDirection, short driveId, DateTimeOffset? date) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/rit/{driveId}?datum={date.ToDeLijnDateOnlyString(true)}";
+        AddOptionalParameters($"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/rit/{driveId}", ("datum", date.ToDeLijnDateOnlyString(true)));
 
-    internal static string GetDisruptionsForLineDirection(short entityId, short lineId, Direction lineDirection, short driveId, DateTimeOffset? date) =>
-        $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/storingen?datum={date.ToDeLijnDateOnlyString(true)}";
+    // internal static string GetDisruptionsForLineDirection(short entityId, short lineId, Direction lineDirection, short driveId, DateTimeOffset? date) =>
+    //     $"{GetAllLines}/{entityId}/{lineId}/lijnrichtingen/{lineDirection.ToTranslatedString()}/storingen?datum={date.ToDeLijnDateOnlyString(true)}";
 
-    private static string AddOptionalParameters(string baseUri, params IEnumerable<KeyValuePair<string, string?>> optionalParams)
+    private static string AddOptionalParameters(string baseUri, params IEnumerable<(string paramName, string? paramValue)> optionalParams)
     {
-        optionalParams = optionalParams.Where(kvp => kvp.Value is not null);
+        optionalParams = optionalParams.Where(kvp => kvp.paramValue is not null);
 
         if (optionalParams.Any())
             return string.Empty;
@@ -144,11 +144,11 @@ internal static class ApiEndpoints
         {
             var stringBuilder = new StringBuilder($"{baseUri}?");
 
-            stringBuilder.Append($"{optionalParams.ElementAt(0).Key}={optionalParams.ElementAt(0).Value}");
+            stringBuilder.Append($"{optionalParams.ElementAt(0).paramName}={optionalParams.ElementAt(0).paramValue}");
 
             for (int i = 1; i < optionalParams.Count(); i++)
             {
-                stringBuilder.Append($"&{optionalParams.ElementAt(i).Key}={optionalParams.ElementAt(i).Value}");
+                stringBuilder.Append($"&{optionalParams.ElementAt(i).paramName}={optionalParams.ElementAt(i).paramValue}");
             }
 
             return stringBuilder.ToString();
